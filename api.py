d00cbce66f822e2061a13a19808d3e56f835e3d0
Ubuntu <ubuntu@ip-172-31-37-231.eu-west-1.compute.internal>
2021-08-14 16:49:54 +0000

    commit api with authentication

--- api.py
+++ api.py
@@ -1,3 +1,6 @@
+#curl -i -X GET http://Steven:5998@0.0.0.0:5002/
+#curl -H "Authorization: Basic U3RldmVuOjU5OTg=" -X GET http://0.0.0.0:5002/
+#echo -n Steven:5998 | base64
 import random
 import pandas as pd
 from flask import Flask
@@ -5,6 +8,8 @@ import json
 from flask import jsonify
 from flask import make_response
 from flask import abort, request
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 import requests
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pickle
@@ -21,6 +26,15 @@ nltk.download('stopwords')
 stopwords = set(stopwords.words("english"))
 stopwords.update(["park", "ride", "rides", "day", "get", "time", "disney", "disneyland", "one", "queue", "go", "food", "paris"])
 
+##def users
+##creation dico users et dico permissions
+dico_users = {}
+dico_perms = {}
+dico_credentials = pd.read_csv('credentials.csv').to_dict()
+for key in dico_credentials['username'].keys():
+    dico_users[dico_credentials['username'][key]]=generate_password_hash(str(dico_credentials['password'][key]))
+    dico_perms[dico_credentials['username'][key]]=[dico_credentials['v1'][key],dico_credentials['v2'][key]]
+
 ##fonction utiles
 def save_obj(obj, name):
     with open(name+'.pkl', 'wb') as f:
@@ -29,25 +43,6 @@ def save_obj(obj, name):
 def load_obj(name):
     with open(name+'.pkl', 'rb') as f:
         return pickle.load(f)
-##fonctions utiles
-def perm_value(username, password):
-    df_cred = pd.read_csv("./credentials.csv") #dataframe des credentials
-    if any(df_cred['username']==str(username)) and any(df_cred['password']==int(password)):#le username et le password sont bien référencés ds les credentials
-        indx_usr = df_cred.loc[(df_cred['username']==str(username))].index[0] #index de la ligne à laquelle se trouve le username recherché
-        indx_pwd = df_cred.loc[(df_cred['password']==int(password))].index[0] #index de la ligne à laquelle se trouve le password recherché
-        if indx_usr==indx_pwd:#le username et le password matchent
-            v1_col, v2_col = 2,3 #index des colonnes v1 et v2 dans le dataframe
-            indx = df_cred.loc[(df_cred['username']==str(username)) & (df_cred['password']==int(password))].index[0] #index de la ligne à laquelle se trouve la paire (username, password) recherchée
-            v1 = df_cred.iloc[indx, v1_col] #valeur de v1 pour cette paire (username, password) (= 0 ou 1)
-            v2 = df_cred.iloc[indx, v2_col] #valeur de v2 pour cette paire (username, password) (= 0 ou 1
-            return v1, v2
-        else :#le username et le password ne matchent pas
-            abort(make_response(jsonify({'error': 'check username and/or password'}), 403))
-    else :#le username et/ou le password ne sont pas référencés ds les credentials
-        abort(make_response(jsonify({'error': 'check username and/or password'}), 403))
-
-def perm_resp(username, v1, v2):
-    return make_response(jsonify({'username': username, 'v1':bool(v1), 'v2':bool(v2)}), 200)
 
 #fonction de scoring de sentiment d'une review
 #score[-1, +1] (sentiment négatif: score<0 / sentiment positif: score>0)
@@ -76,24 +71,51 @@ def score(review):
         score = (probas[0][1]-0.5) *2 #score sentiment positif : de 0 à +1
     return score
 
-
 ##API
 host="0.0.0.0"
 api = Flask(import_name='my_api')
+#app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in dico_users and \
+            check_password_hash(dico_users.get(username), password):
+        return username
+
+def perm_value():
+    v1,v2 = dico_perms[str(auth.current_user())] 
+    return make_response(jsonify({'username': str(auth.current_user()), 'v1':bool(v1), 'v2':bool(v2)}), 200)
+
 
 @api.route('/status', methods=['GET']) #renvoie 1 si l’API fonctionne
 def api_in_use():
     return  make_response(jsonify({'it works!': '1'}), 200)
 
-@api.route('/permissions/<username>/<password>', methods=['GET']) #renvoie la liste des permissions d’un utilisateur authentifié par son username et son password
-def perm_type(username, password):
-    v1, v2 = perm_value(username, password)
-    perm_json = perm_resp(username, v1, v2)
+@api.route('/permissions', methods=['GET']) #renvoie la liste des permissions d’un utilisateur authentifié par son username et son password
+@auth.login_required
+def perm_type():
+    #    v1, v2 = perm_value()
+    perm_json = perm_value()
+#    perm_json = perm_json.json()
     return perm_json
 
-@api.route('/v1/sentiment/<username>/<password>/<sentence>', methods=['GET']) #renvoie le score de sentiment v1 de la phrase proposée
-def v1_resp(username, password, sentence):
-    v1, v2 = perm_value(username, password)
+@api.route('/v1/test', methods=['GET']) #renvoie les performance de l'algorithme sur le jeu de test
+@auth.login_required
+def v1_test_score():
+    v1,v2 = dico_perms[str(auth.current_user())]
+    if v1==1:
+        model1_test_score = load_obj('model1_test_score')
+        return  make_response(jsonify({"score model1 sur jeu de test": round(model1_test_score,4)}), 200)
+    elif v1==0:
+        abort(make_response(jsonify({'error': 'check v1 acces'}), 403))
+    else:
+        abort(make_response(jsonify({'error': 'check v1 value'}), 400))
+
+@api.route('/v1/sentiment/<sentence>', methods=['GET']) #renvoie le score de sentiment v1 de la phrase proposée
+@auth.login_required
+def v1_resp(sentence):
+    v1,v2 = dico_perms[str(auth.current_user())]
     if v1==1:
         score_v1 = round(score(sentence), 4)
         return  make_response(jsonify({"sentence_sentiment_score": score_v1}), 200)
@@ -102,9 +124,10 @@ def v1_resp(username, password, sentence):
     else:
         abort(make_response(jsonify({'error': 'check v1 value'}), 400))
 
-@api.route('/v2/sentiment/<username>/<password>/<sentence>', methods=['GET']) #renvoie le score de sentiment v2 de la phrase proposée
-def v2_resp(username, password, sentence):
-    v1, v2 = perm_value(username, password)
+@api.route('/v2/sentiment/<sentence>', methods=['GET']) #renvoie le score de sentiment v2 de la phrase proposée
+@auth.login_required
+def v2_resp(sentence):
+    v1,v2 = dico_perms[str(auth.current_user())]
     if v2==1:
         analyzer = SentimentIntensityAnalyzer()
         sentence = sentence.replace("_", " ").replace("%", " ")
